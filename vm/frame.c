50d370f7f51d5902bad32a9c3c6877e48ddcce4e
baochenhu <baochenhu@gmail.com>
2015-03-30 23:43:38 -0400

    5 left

--- vm/frame.c
+++ vm/frame.c
@@ -97,5 +97,50 @@ frame_add_to_table (void *frame, struct sup_page_entry *spte)
 void *
 frame_evict (enum palloc_flags flags)
 {
-
+     lock_acquire(&frame_table_lock);
+     struct list_elem *e = list_begin(&frame_table);
+  
+  while (true)
+    {
+      struct frame_entry *fte = list_entry(e, struct frame_entry, elem);
+      if (!fte->spte->pinned)
+	{
+	  struct thread *t = fte->thread;
+	  if (pagedir_is_accessed(t->pagedir, fte->spte->uva))
+	    {
+	      pagedir_set_accessed(t->pagedir, fte->spte->uva, false);
+	    }
+	  else
+	    {
+	      if (pagedir_is_dirty(t->pagedir, fte->spte->uva) ||
+		  fte->spte->type == SWAP)
+		{
+		  if (fte->spte->type == MMAP)
+		    {
+		      lock_acquire(&file_lock);
+		      file_write_at(fte->spte->file, fte->frame,
+				    fte->spte->read_bytes,
+				    fte->spte->offset);
+		      lock_release(&file_lock);
+		    }
+		  else
+		    {
+		      fte->spte->type = SWAP;
+		      fte->spte->swap_index = swap_out(fte->frame);
+		    }
+		}
+	      fte->spte->is_loaded = false;
+	      list_remove(&fte->elem);
+	      pagedir_clear_page(t->pagedir, fte->spte->uva);
+	      palloc_free_page(fte->frame);
+	      free(fte);
+	      return palloc_get_page(flags);
+	    }
+	}
+      e = list_next(e);
+      if (e == list_end(&frame_table))
+	{
+	  e = list_begin(&frame_table);
+	}
+    }
 }
